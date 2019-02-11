79fd2e2f5c1d107ff933cfd66643cf5c7bc25658
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-11 19:54:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+<<<<<<< HEAD
   attendees.map { |i| badge_maker(i)}
 end
 
@@ -16,4 +17,9 @@ end
 def printer(array)
   batch_badge_creator(array).each {|i| puts i}
   assign_rooms(array).each {|i| puts i}
+=======
+  attendees.each do |i|
+    badge_maker(i)
+  end
+>>>>>>> 4a75600bc481aaa0392704eaecb208775bb5f4b9
 end
